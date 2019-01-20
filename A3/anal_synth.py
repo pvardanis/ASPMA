2484bdf6d06eb23506ccf90b353e0b7c755c0934
pvardanis <panosvardanis@gmail.com>
2019-01-20 22:46:22 +0200

    week 3

---
+++ A3/anal_synth.py
@@ -0,0 +1,20 @@
+import numpy as np
+from scipy.signal import get_window
+import os,sys
+sys.path.insert(0, '/home/pvardanis/sms-tools/software/models/')
+import utilFunctions as UF
+import dftModel as DFT
+
+(fs, x) = UF.wavread('../../sounds/piano.wav')
+
+M = 511
+w = get_window('hamming', M)
+
+time = .2
+x1 = x[int(time*fs):int(time*fs) + M]
+
+N = 1024
+mX, pX = DFT.dftAnal(x1, w, N)
+
+y = DFT.dftSynth(mX, pX, w.size)*sum(w) #because it's normalized
+
---
+++ A3/fourier2.py
@@ -0,0 +1,26 @@
+"""%reload_ext autoreload
+%autoreload 2"""
+
+
+
+import numpy as np
+from scipy.fftpack import fft
+import sys, os, math
+sys.path.insert(0, '/home/pvardanis/sms-tools/software/models/')
+import utilFunctions as UF 
+
+M = 501
+hM1 = int(math.floor((M+1)/2))
+hM2 = int(math.floor(M/2))
+
+(fs, x) = UF.wavread('/home/pvardanis/sms-tools/sounds/soprano-E4.wav')
+x1 = x[5000:5000+M] * np.hamming(M)
+
+N = 1024
+fftbuffer = np.zeros(N)
+fftbuffer[:hM1] = x1[hM2:]
+fftbuffer[N-hM2:] = x1[:hM2]
+
+X = fft(fftbuffer)
+mX = 20*np.log10(abs(X))
+pX = np.unwrap(np.angle(X))
